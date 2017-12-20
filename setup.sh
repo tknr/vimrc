56cf1d624f7a6280c17c406fd3557c68cf14d0c9
tknr <info@tknr.com>
2017-12-20 17:37:47 +0900

    2017年 12月 20日 水曜日 17:37:47 JST

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp -v .dein.toml ~/
-cp -v .dein_lazy.toml ~/
-cp -v .gvimrc ~/
-cp -v .vimrc ~/
+ln -s `pwd`/.dein.toml ~/
+ln -s `pwd`/.dein_lazy.toml ~/
+ln -s `pwd`/.gvimrc ~/
+ln -s `pwd`/.vimrc ~/
 
